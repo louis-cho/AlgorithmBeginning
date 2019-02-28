4a4b0aa6cc3cde11206e8b99e0c329d0c6c3aff2
louis-cho <wizardchw@naver.com>
2019-02-28 20:16:02 +0900

    DynamicProgramming

---
+++ 프로젝트/DP/BellNum/BellNumber.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void) {
+	int** arr = NULL;
+	int temp;
+	printf("몇 번째 Bell Number를 구할 것 인가요? >> ");
+	scanf("%d", &temp);
+
+	int n = temp - 1;
+	arr = (int**)malloc(sizeof(int*)*n);
+	for (int i = 0; i < n; i++) {
+		arr[i] = (int*)malloc(sizeof(int)*(i + 1));
+	}
+	arr[0][0] = 1;
+	for (int i = 1; i < n; i++) {
+		for (int j = 1; j <= i; j++) {
+			if (j == 1) {
+				arr[i][0] = arr[i - 1][i - 1];
+			}
+			arr[i][j] = arr[i - 1][j - 1] + arr[i][j-1];
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <= i; j++) {
+			printf("%d ", arr[i][j]);
+		} printf("\n");
+	}
+	printf("\n\nBell Number is %d\n", arr[n-1][n-1]);
+	for (int i = 0; i < n; i++) {
+		free(arr[i]);
+	}
+	free(arr);
+
+	return 0;
+}
